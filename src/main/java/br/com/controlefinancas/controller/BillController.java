23fa1f068ba57b06964f01232076902c99ca3546
Gabriel da Fonseca Meira <gabriel.gfm@outlook.com>
2024-06-22 14:59:49 -0300

    funfando

--- src/main/java/br/com/controlefinancas/controller/BillController.java
+++ src/main/java/br/com/controlefinancas/controller/BillController.java
@@ -2,6 +2,7 @@ package br.com.controlefinancas.controller;
 
 import br.com.controlefinancas.entity.Bill;
 import br.com.controlefinancas.entity.BillType;
+import br.com.controlefinancas.entity.Owner;
 import br.com.controlefinancas.service.BillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +29,11 @@ public class BillController {
     public Bill getBillById(@PathVariable(value = "id") Long id){
         Bill bill = billService.findById(id);
         return bill;
+    }
+
+    @GetMapping("/bills/{owner}")
+    public List<Bill> getBillByOwner(Owner owner){
+        return billService.findByOwner(owner);
     }
 
     @PostMapping("/bills")
